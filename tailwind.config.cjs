daae52c232b2f0235fcc73ccd15f7d960eb50253
Valto Silva <valtosilva7@gmail.com>
2023-02-13 16:54:26 -0300

    Revert "02 Login and register page"
    This reverts commit d01aecabb1fdbbca054bf7416343239438b27d39.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,11 +2,7 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: "#F5385D",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
